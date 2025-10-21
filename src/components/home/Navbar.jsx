9c59e2cdee2e035e4bc465aa18ba243ee43d60ae
Temidayo Gabriel <dayotoru@gmail.com>
2025-10-21 18:34:54 -0400

    add navigation button to navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -27,7 +28,7 @@ const Navbar = () => {
               <a
                 key={link.name}
                 href={link.href}
-                className="hover:text-rose-500 font-medium transition-all duration-300 ease-in-out"
+                className="hover:text-primary font-medium transition-all duration-300 ease-in-out"
               >
                 {link.name}
               </a>
@@ -36,29 +37,28 @@ const Navbar = () => {
 
           {/* Desktop Right Side */}
           <div className="hidden md:flex items-center space-x-4">
-            <div className="relative">
-              <Button variant="ghost" className="relative group">
-                <ShoppingCart className="size-6 text-gray-700 group-hover:text-rose-500 transition-colors duration-300" />
-                <span className="absolute -top-1 -right-1 bg-rose-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
-                  0
-                </span>
-              </Button>
-            </div>
+            <Button variant="ghost" className="relative group text-xl">
+              Cart
+              <ShoppingCart className="size-6 text-gray-700 group-hover:text-primary transition-colors duration-300" />
+              <span className="absolute -top-1 -right-1 bg-primary text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                0
+              </span>
+            </Button>
 
-            <a
-              href="#mission"
-              className="bg-rose-500 text-white px-6 py-2 rounded-full font-semibold hover:bg-rose-600 transition-all duration-300 transform hover:scale-105 flex items-center space-x-2"
+            <Link
+              to="/login"
+              className="bg-primary text-white px-6 py-1.5 rounded-full font-semibold hover:bg-rose-600 transition-all duration-300 transform hover:scale-105 flex items-center space-x-2"
             >
-              <span>Learn More</span>
-            </a>
+              <span>Login</span>
+            </Link>
           </div>
 
           {/* Mobile Right Side */}
           <div className="flex md:hidden items-center space-x-3">
             {/* Cart on Mobile */}
-            <button className="relative p-2 text-gray-700 hover:text-rose-500 transition-colors duration-300">
+            <button className="relative p-2 text-gray-700 hover:text-primary transition-colors duration-300">
               <ShoppingCart className="size-6.5" />
-              <span className="absolute -top-1 -right-1 bg-rose-500 text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">
+              <span className="absolute -top-1 -right-1 bg-primary text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">
                 0
               </span>
             </button>
@@ -92,7 +92,7 @@ const Navbar = () => {
         {/* Mobile Menu */}
         <div
           className={`md:hidden transition-all duration-300 ease-in-out overflow-hidden ${
-            isMenuOpen ? "max-h-64 opacity-100 py-4" : "max-h-0 opacity-0"
+            isMenuOpen ? "max-h-74 opacity-100 py-4" : "max-h-0 opacity-0"
           }`}
         >
           <div className="flex flex-col space-y-4">
@@ -100,20 +100,18 @@ const Navbar = () => {
               <a
                 key={link.name}
                 href={link.href}
-                className="hover:text-rose-500 font-medium py-2 text-left transition-colors duration-300"
+                className="hover:text-primary font-medium py-2 text-left transition-colors duration-300"
               >
                 {link.name}
               </a>
             ))}
-            <a
-              href="https://wa.me/2349012345678"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-rose-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-rose-600 transition-colors duration-300 flex items-center justify-center space-x-2 mt-4"
+
+            <Link
+              to="/login"
+              className="bg-primary w-22 text-white text-center py-1.5 rounded-full font-semibold hover:bg-rose-600 transition-all duration-300 transform"
             >
-              <span>💬</span>
-              <span>Chat on WhatsApp</span>
-            </a>
+              <span>Login</span>
+            </Link>
           </div>
         </div>
       </div>
